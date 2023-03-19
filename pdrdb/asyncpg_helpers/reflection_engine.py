8342de0a171a83fb761d054a1aaa6e97644e855e
Aron Braun <abraunu@gmail.com>
2023-03-19 12:28:56 -0400

    enable to use the reflect function with a dsn param instead of an engin

--- pdrdb/asyncpg_helpers/reflection_engine.py
+++ pdrdb/asyncpg_helpers/reflection_engine.py
@@ -2,7 +2,7 @@ import types
 from typing import Union
 
 import sqlalchemy.dialects.postgresql.asyncpg
-from sqlalchemy import MetaData, event
+from sqlalchemy import MetaData, event, create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.automap import automap_base
 
@@ -23,11 +23,18 @@ def receive_column_reflect(_inspector, table, column_info):
 Base: sqlalchemy.ext.automap.AutomapBase = automap_base(metadata=metadata)
 
 
-def reflect(db_engine: Engine, schemas: Union[str, list, tuple], custom_types_module: types.ModuleType = None):
+def reflect(db: Engine | str, schemas: Union[str, list, tuple], custom_types_module: types.ModuleType = None):
+    if isinstance(db, str):
+        dsn = db
+        db = create_engine(
+            dsn.replace('postgresql+asyncpg://', 'postgresql://'),
+            connect_args={"options": "-csearch_path=public"},
+        )
+
     if isinstance(schemas, str):
         schemas = [s.strip() for s in schemas.split(',') if s]
-    reflect_load_composite_types(db_engine=db_engine, schemas=None, custom_types_module=custom_types_module)
+    reflect_load_composite_types(db_engine=db, schemas=None, custom_types_module=custom_types_module)
 
     for schema in schemas:
-        metadata.reflect(schema=schema, views=True, bind=db_engine)
-    db_engine.dispose()
+        metadata.reflect(schema=schema, views=True, bind=db)
+    db.dispose()
